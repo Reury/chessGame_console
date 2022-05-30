c853c7ca6ebbe0c7c7bcb7cd595faed82e7f96a1
Reury araujo <reuryaraujo@gmail.com>
2022-05-30 11:02:46 -0300

    fisrt commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ChessGame.board;
+
+namespace ChessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p = new Position(3,4);
+
+            Console.WriteLine("Posicao: " + p);
+            
+
+        }
+    }
+}
---
+++ board/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.board
+{
+    internal class Position
+    {
+        public int row { get; set; }
+        public int col { get; set; }
+
+        public Position(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+        public override string ToString()
+        {
+            return row +  ", " + col;
+        }
+    }
+}
